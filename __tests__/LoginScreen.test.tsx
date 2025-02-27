76874aad5962662b72ba1fba45c73a87995920ab
jep <n.siomusic@gmail.com>
2025-02-27 07:16:11 +0700

    [COVERAGE] Improve LoginScreen tests and include test coverage results

--- __tests__/LoginScreen.test.tsx
+++ __tests__/LoginScreen.test.tsx
@@ -1,11 +1,38 @@
 import React from "react";
-import { render } from "@testing-library/react-native";
+import { render, fireEvent } from "@testing-library/react-native";
 import LoginScreen from "@/app/index";
+import { Alert } from "react-native";
+
+jest.spyOn(Alert, "alert");
 
 describe("LoginScreen", () => {
   it("renders the LoginForm component", () => {
     const { getByTestId } = render(<LoginScreen />);
     
     expect(getByTestId("login-screen")).toBeTruthy();
+  });
+
+  it("triggers forgot password alert when the button is pressed", () => {
+    const { getByText } = render(<LoginScreen />);
+    
+    fireEvent.press(getByText("Forgot password?"));
+    
+    expect(Alert.alert).toHaveBeenCalledWith("Forgot Password Pressed!");
+  });
+
+  it("triggers sign up alert when the sign-up text is pressed", () => {
+    const { getByText } = render(<LoginScreen />);
+    
+    fireEvent.press(getByText("Sign up."));
+    
+    expect(Alert.alert).toHaveBeenCalledWith("Sign Up Pressed!");
+  });
+
+  it("triggers log in alert when the login button is pressed", () => {
+    const { getByTestId } = render(<LoginScreen />);
+    
+    fireEvent.press(getByTestId("login-button"));
+    
+    expect(Alert.alert).toHaveBeenCalledWith("Log In Pressed!");
   });
 });
--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,13 @@
-import { View } from "react-native";
+import { View, Alert } from "react-native";
 import LoginForm from "@/components/LoginForm";
 
 export default function LoginScreen() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <LoginForm testID="login-screen"
-        onForgotPassword={() => alert("Forgot Password Pressed!")} 
-        onSignUp={() => alert("Sign Up Pressed!")} 
-        onLogIn={() => alert("Log In Pressed!")} 
+        onForgotPassword={() => Alert.alert("Forgot Password Pressed!")} 
+        onSignUp={() => Alert.alert("Sign Up Pressed!")} 
+        onLogIn={() => Alert.alert("Log In Pressed!")} 
       />
     </View>
   );
