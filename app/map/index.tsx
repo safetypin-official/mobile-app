06cb113f2605d4db708b4a08a39aa84498749ec2
jep <n.siomusic@gmail.com>
2025-03-09 18:33:41 +0700

    [GREEN] Add custom map view import into map index page, generate coverage

--- app/map/index.tsx
+++ app/map/index.tsx
@@ -1,13 +1,21 @@
 import React from "react";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 import SearchBar from "@/components/inputs/SearchBar";
+import CustomMapView from "@/components/views/CustomMapView";
 
 const MapScreen = () => {
   return (
-    <View testID="map-screen">
+    <View style={styles.container} testID="map-screen">
       <SearchBar />
+      <CustomMapView />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default MapScreen;
