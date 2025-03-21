94024c2449205807fcd7e0c2d44ff2f012acba2f
Muhammad Raihan Akbar <ianakbar711@gmail.com>
2025-03-21 07:17:14 +0700

    [RED] Update Pin component test

--- __tests__/assets/Pin.test.tsx
+++ __tests__/assets/Pin.test.tsx
@@ -26,61 +26,85 @@ describe('Pin Component', () => {
     });
 
     it('renders correctly with lost-item type', () => {
-        const { getByTestId } = render(<Pin type="lost-item" onPress={mockOnPress} />);
+        const { getByTestId } = render(<Pin type="Lost Item" onPress={mockOnPress} />);
         const pin = getByTestId('pin-svg');
         expect(pin.props.xml).toBe(lostItemPin);
     });
 
     it('renders correctly with found-item type', () => {
-        const { getByTestId } = render(<Pin type="found-item" onPress={mockOnPress} />);
+        const { getByTestId } = render(<Pin type="Found Item" onPress={mockOnPress} />);
         const pin = getByTestId('pin-svg');
         expect(pin.props.xml).toBe(foundItemPin);
     });
 
     it('renders correctly with theft type', () => {
-        const { getByTestId } = render(<Pin type="theft" onPress={mockOnPress} />);
+        const { getByTestId } = render(<Pin type="Theft" onPress={mockOnPress} />);
         const pin = getByTestId('pin-svg');
         expect(pin.props.xml).toBe(theftPin);
     });
 
     it('renders correctly with harassment type', () => {
-        const { getByTestId } = render(<Pin type="harassment" onPress={mockOnPress} />);
+        const { getByTestId } = render(<Pin type="Harassment" onPress={mockOnPress} />);
         const pin = getByTestId('pin-svg');
         expect(pin.props.xml).toBe(harassmentPin);
     });
 
     it('renders correctly with flood type', () => {
-        const { getByTestId } = render(<Pin type="flood" onPress={mockOnPress} />);
+        const { getByTestId } = render(<Pin type="Flood" onPress={mockOnPress} />);
         const pin = getByTestId('pin-svg');
         expect(pin.props.xml).toBe(floodPin);
     });
 
     it('renders correctly with assault type', () => {
-        const { getByTestId } = render(<Pin type="assault" onPress={mockOnPress} />);
+        const { getByTestId } = render(<Pin type="Assault" onPress={mockOnPress} />);
         const pin = getByTestId('pin-svg');
         expect(pin.props.xml).toBe(assaultPin);
     });
 
     it('renders correctly with fire type', () => {
-        const { getByTestId } = render(<Pin type="fire" onPress={mockOnPress} />);
+        const { getByTestId } = render(<Pin type="Fire" onPress={mockOnPress} />);
         const pin = getByTestId('pin-svg');
         expect(pin.props.xml).toBe(firePin);
     });
 
     it('renders correctly with other-disaster type', () => {
-        const { getByTestId } = render(<Pin type="other-disaster" onPress={mockOnPress} />);
+        const { getByTestId } = render(<Pin type="Other Disaster" onPress={mockOnPress} />);
         const pin = getByTestId('pin-svg');
         expect(pin.props.xml).toBe(otherDisasterPin);
     });
 
     it('renders correctly with earthquake type', () => {
-        const { getByTestId } = render(<Pin type="earthquake" onPress={mockOnPress} />);
+        const { getByTestId } = render(<Pin type="Earthquake" onPress={mockOnPress} />);
         const pin = getByTestId('pin-svg');
         expect(pin.props.xml).toBe(earthquakePin);
     });
 
     it('renders correctly with other-crime type', () => {
-        const { getByTestId } = render(<Pin type="other-crime" onPress={mockOnPress} />);
+        const { getByTestId } = render(<Pin type="Other Crime" onPress={mockOnPress} />);
+        const pin = getByTestId('pin-svg');
+        expect(pin.props.xml).toBe(otherCrimePin);
+    });
+
+    it('renders correctly with lost book type', () => {
+        const { getByTestId } = render(<Pin type="Lost Book" onPress={mockOnPress} />);
+        const pin = getByTestId('pin-svg');
+        expect(pin.props.xml).toBe(lostItemPin);
+    });
+
+    it('renders correctly with lost pet type', () => {
+        const { getByTestId } = render(<Pin type="Lost Pet" onPress={mockOnPress} />);
+        const pin = getByTestId('pin-svg');
+        expect(pin.props.xml).toBe(lostItemPin);
+    });
+
+    it('renders correctly with infrastructure issue type', () => {
+        const { getByTestId } = render(<Pin type="Infrastructure Issue" onPress={mockOnPress} />);
+        const pin = getByTestId('pin-svg');
+        expect(pin.props.xml).toBe(otherDisasterPin);
+    });
+
+    it('renders correctly with other type', () => {
+        const { getByTestId } = render(<Pin type="Something" onPress={mockOnPress} />);
         const pin = getByTestId('pin-svg');
         expect(pin.props.xml).toBe(otherCrimePin);
     });
