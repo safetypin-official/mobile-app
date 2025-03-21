4045a74ff5cbcbce747b93ccca77e51a946797fe
Muhammad Raihan Akbar <ianakbar711@gmail.com>
2025-03-21 09:15:16 +0700

    [RED] Update failing test for Tags

--- __tests__/components/enum/Tags.test.tsx
+++ __tests__/components/enum/Tags.test.tsx
@@ -1,43 +1,67 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react-native";
-import Tag from "../../../components/displays/Tags";
+import Tag from "@/components/displays/post/Tags";
+import {
+  lostItemTag,
+  foundItemTag,
+  theftTag,
+  harassmentTag,
+  floodTag,
+  assaultTag,
+  fireTag,
+  otherDisasterTag,
+  earthquakeTag,
+  otherCrimeTag
+} from '../../../assets/tags';
+
+// Mock react-native-svg
+jest.mock('react-native-svg', () => {
+  const { View } = require('react-native');
+  return {
+    SvgXml: ({ xml, style }) => (
+      <View data-xml={xml} style={style} />
+    ),
+  };
+});
 
 const tagTypes = [
-  "lost-item",
-  "found-item",
-  "theft",
-  "harassment",
-  "flood",
-  "assault",
-  "fire",
-  "other-disaster",
-  "earthquake",
-  "other-crime",
+  { type: "lost-item", xml: lostItemTag },
+  { type: "found-item", xml: foundItemTag },
+  { type: "theft", xml: theftTag },
+  { type: "harassment", xml: harassmentTag },
+  { type: "flood", xml: floodTag },
+  { type: "assault", xml: assaultTag },
+  { type: "fire", xml: fireTag },
+  { type: "other-disaster", xml: otherDisasterTag },
+  { type: "earthquake", xml: earthquakeTag },
+  { type: "other-crime", xml: otherCrimeTag },
 ];
 
 describe("Tag Component", () => {
   const mockOnPress = jest.fn();
 
-  test("renders default tag when no type is provided", () => {
-    const { getByTestId } = render(<Tag onPress={mockOnPress} />);
-    expect(getByTestId("tag-icon-other-crime")).toBeTruthy();
+  beforeEach(() => {
+    mockOnPress.mockClear();
   });
 
-  tagTypes.forEach((type) => {
+  test("renders default tag (other-crime) when no type is provided", () => {
+    const { UNSAFE_getByProps } = render(<Tag onPress={mockOnPress} />);
+    const svgElement = UNSAFE_getByProps({ 'data-xml': otherCrimeTag });
+    expect(svgElement).toBeTruthy();
+    expect(svgElement.props.style).toEqual({ width: 6, height: 6 });
+  });
+
+  tagTypes.forEach(({ type, xml }) => {
     test(`renders correct tag for type "${type}"`, () => {
-      const { getByTestId } = render(<Tag type={type} onPress={mockOnPress} />);
-      expect(getByTestId(`tag-icon-${type}`)).toBeTruthy();
+      const { UNSAFE_getByProps } = render(<Tag type={type} onPress={mockOnPress} />);
+      const svgElement = UNSAFE_getByProps({ 'data-xml': xml });
+      expect(svgElement).toBeTruthy();
     });
   });
 
-  test("renders default tag when an invalid type is provided", () => {
-    const { getByTestId } = render(<Tag type="invalid-type" onPress={mockOnPress} />);
-    expect(getByTestId("tag-icon-other-crime")).toBeTruthy();
-  });
-
-  test("calls onPress when tag is clicked", () => {
-    const { getByTestId } = render(<Tag type="fire" onPress={mockOnPress} />);
-    fireEvent.press(getByTestId("tag-button"));
-    expect(mockOnPress).toHaveBeenCalledTimes(1);
+  test("renders default tag (other-crime) when an invalid type is provided", () => {
+    const { UNSAFE_getByProps } = render(<Tag type="invalid-type" onPress={mockOnPress} />);
+    const svgElement = UNSAFE_getByProps({ 'data-xml': otherCrimeTag });
+    expect(svgElement).toBeTruthy();
   });
 });
