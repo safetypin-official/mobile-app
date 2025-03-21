fadd4174120464c61685bb3e0e795048219bca34
Muhammad Raihan Akbar <ianakbar711@gmail.com>
2025-03-21 07:17:14 +0700

    [RED] Update InputField component test

--- __tests__/components/inputs/InputField.test.tsx
+++ __tests__/components/inputs/InputField.test.tsx
@@ -20,4 +20,22 @@ describe("InputField Component", () => {
     const input = getByPlaceholderText("Enter password");
     expect(input.props.secureTextEntry).toBe(true);
   });
+
+  it("sets numberOfLines to 1 when multiline is false", () => {
+    const { getByPlaceholderText } = render(
+      <InputField placeholder="Single-line input" multiline={false} />
+    );
+
+    const input = getByPlaceholderText("Single-line input");
+    expect(input.props.numberOfLines).toBe(1);
+  });
+
+  it("sets numberOfLines to 5 when multiline is true", () => {
+    const { getByPlaceholderText } = render(
+      <InputField placeholder="Multi-line input" multiline />
+    );
+
+    const input = getByPlaceholderText("Multi-line input");
+    expect(input.props.numberOfLines).toBe(5);
+  });
 });
