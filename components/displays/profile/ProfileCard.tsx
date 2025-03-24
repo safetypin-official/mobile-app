06833dacf18ee6f8cdd9a1a81184d0ba31bd04ca
stronovski <andisalsabilaardian@gmail.com>
2025-03-24 16:23:28 +0700

    [RED] Add tests for ProfileCard component

---
+++ components/displays/profile/ProfileCard.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View } from 'react-native';
+
+
+interface ProfileCardProps {
+
+}
+
+const ProfileCard: React.FC<ProfileCardProps> = ({
+
+}) => {
+  return (
+    <View>
+
+    </View>
+  );
+};
+
+export default ProfileCard
