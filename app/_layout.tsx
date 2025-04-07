817e0f6e622edacb14c557ef9e9203e781ad0eb0
Muhammad Raihan Akbar <ianakbar711@gmail.com>
2025-04-08 01:56:13 +0700

    [REFACTOR] Add routes to profile

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -115,6 +115,7 @@ export default function Layout() {
           }}
           onProfilePress={() => {
             setActiveTab("profile");
+            router.replace("/profile");
           }}
         />
       )}
