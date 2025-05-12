f86f32674ab82c1d26e84d55cc478dbe76f01522
stronovski <andisalsabilaardian@gmail.com>
2025-05-12 17:57:56 +0700

    [REFACTOR] Fix implementation for sonarqube security issues

--- components/displays/PostsTabs.tsx
+++ components/displays/PostsTabs.tsx
@@ -150,7 +150,7 @@ const PostsTabs: React.FC<PostsTabsProps> = ({
     // Find the active tab configuration
     const activeTabConfig = tabs.find(tab => tab.key === activeTab);
     
-    if (activeTabConfig && activeTabConfig.refreshTrigger !== undefined) {
+    if (activeTabConfig?.refreshTrigger !== undefined) {
       // If there's a refreshTrigger property, reload data when it changes
       loadPosts(activeTab, 0, true);
     }
